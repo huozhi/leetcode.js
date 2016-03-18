7ef545d222428d92b8d25521a4e35f35e49180b2
huozhi <inbox@huozhi.im>
2016-03-18 23:44:36 +0800

    word-pattern

---
+++ __test__/word-pattern.test.js
@@ -0,0 +1,17 @@
+import {expect} from 'chai'
+import {wordPattern} from '../src/word-pattern'
+
+describe('word pattern', () => {
+  it('1', () => {
+    expect(wordPattern('abba', 'dog cat cat dog')).to.equal(true)
+  })
+  it('2', () => {
+    expect(wordPattern('abba', 'dog cat cat fish')).to.equal(false)
+  })
+  it('3', () => {
+    expect(wordPattern('aaaa', 'dog cat cat fish')).to.equal(false)
+  })
+  it('4', () => {
+    expect(wordPattern('abba', 'dog dog dog dog')).to.equal(false)
+  })
+})
---
+++ src/word-pattern.js
@@ -0,0 +1,48 @@
+/**
+
+Given a pattern and a string str, find if str follows the same pattern.
+
+Here follow means a full match, such that there is a bijection between a letter in pattern and a non-empty word in str.
+
+Examples:
+pattern = "abba", str = "dog cat cat dog" should return true.
+pattern = "abba", str = "dog cat cat fish" should return false.
+pattern = "aaaa", str = "dog cat cat dog" should return false.
+pattern = "abba", str = "dog dog dog dog" should return false.
+Notes:
+You may assume pattern contains only lowercase letters, and str contains lowercase letters separated by a single space.
+ */
+
+/**
+ * @param {string} pattern
+ * @param {string} str
+ * @return {boolean}
+ */
+
+var wordPattern = function(pattern, str) {
+  var pdict = {}, sdict = {}
+  var pv, sv
+  str = str.split(/\ +/)
+  if (pattern.length !== str.length) {
+    return false
+  }
+  for (var len = pattern.length, i = 0; i < len; i++) {
+    pv = pattern[i], sv = str[i]
+    if (!pdict[pv]) {
+      pdict[pv] = sv
+    }
+    else if (pdict[pv] !== sv) {
+      return false
+    }
+
+    if (!sdict[sv]) {
+      sdict[sv] = pv
+    }
+    else if (sdict[sv] !== pv) {
+      return false
+    }
+  }
+  return true
+}
+
+module.exports = {wordPattern}
